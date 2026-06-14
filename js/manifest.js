23a7ffe7d098d51d3e38a743a6852ac65d8b9346
999 <2521700086@qq.com>
2026-06-14 21:38:04 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '696c79772478139af50077fc';
+_mrmcp['creative_id'] = '6989f0acd44afdd585033034';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 625;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '无标题';
+_mrmcp['build_number'] = parseInt('370');
+_mrmcp['publish_time'] = 'Mon Jun 08 2026 01:12:16 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
